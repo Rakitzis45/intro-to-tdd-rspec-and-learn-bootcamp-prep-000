b85db925cd762f4ae21aecec9e1e39ae0238ebcb
Michael Rakitzis <michael.rakitzis@gmail.com>
2020-09-10 18:01:09 +0000

    Automatically backed up by Learn

--- current_age_for_birth_year.rb
+++ current_age_for_birth_year.rb
@@ -1,6 +1,6 @@
 def current_age_for_birth_year(birth_year)
 current_year = Time.now.year
 age_of_person = current_year - birth_year
-return "Your current age is #{age_of_person} years old."
+puts "Your current age is #{age_of_person} years old."
 end
 current_age_for_birth_year(1993)
